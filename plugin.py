fbf2d2a5e1122ed61e44a8e30c839fbce8c3ac02
Raoul Wols <raoulwols@gmail.com>
2020-09-11 18:09:52 +0200

    Start over

---
+++ plugin.py
@@ -0,0 +1,29 @@
+from LSP.plugin import AbstractPlugin
+from LSP.plugin.core.typing import Dict, Optional, Tuple
+import sublime
+
+
+class Lua(AbstractPlugin):
+    @classmethod
+    def name(cls) -> str:
+        return "LSP-{}".format(cls.__name__.lower())
+
+    @classmethod
+    def configuration(cls) -> Tuple[sublime.Settings, str]:
+        base_name = "{}.sublime-settings".format(cls.name())
+        file_path = "Packages/{}/{}".format(cls.name(), base_name)
+        return sublime.load_settings(base_name), file_path
+
+    @classmethod
+    def additional_variables(cls) -> Optional[Dict[str, str]]:
+        settings, _ = cls.configuration()
+        locale = str(settings.get("locale"))
+        binplatform = {
+            "linux": "Linux",
+            "windows": "Windows",
+            "osx": "macOS"
+        }[sublime.platform()]
+        return {
+            "binplatform": binplatform,
+            "locale": locale
+        }
