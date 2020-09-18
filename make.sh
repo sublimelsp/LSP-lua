cee0c4b5d686db544d8dde0f29557f45108d3e04
Raoul Wols <raoulwols@gmail.com>
2020-09-18 17:14:09 +0200

    Use a script

---
+++ make.sh
@@ -0,0 +1,32 @@
+ls -R vscode-lua
+ls -R LSP-lua-source
+
+VSCODE_LUA_DIR="${GITHUB_WORKSPACE}/vscode-lua"
+LSP_LUA_SOURCE_DIR="${GITHUB_WORKSPACE}/LSP-lua-source"
+
+function process
+{
+    local OUT_DIR="${GITHUB_WORKSPACE}/out/LSP-lua-$1"
+    local BIN_DIR="${VSCODE_LUA_DIR}/bin/$2"
+    mkdir -p out/$1
+    pushd "${OUT_DIR}"
+        mkdir -p bin/$2
+        cp "${BIN_DIR}"/* bin/$2/
+        chmod +x bin/$2/*
+        cp -R "${VSCODE_LUA_DIR}/server/scripts" .
+        cp "${VSCODE_LUA_DIR}/server/main.lua" .
+        cp "${LSP_LUA_SOURCE_DIR}/plugin.py" .
+        cp "${LSP_LUA_SOURCE_DIR}/LSP-lua.sublime-commands" .
+        cp "${LSP_LUA_SOURCE_DIR}/LSP-lua.sublime-settings" .
+        cp "${LSP_LUA_SOURCE_DIR}/LICENSE" .
+        cp "${LSP_LUA_SOURCE_DIR}/NOTICE" .
+        touch .no-sublime-package
+    popd
+    zip LSP-lua-$1.zip "${OUT_DIR}"
+}
+
+process linux Linux
+process osx macOS
+process windows Windows
+
+ls -lash
