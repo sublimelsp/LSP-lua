5d96f4d952a6fd832a2a1d101c2b8001d095f50e
Raoul Wols <raoulwols@gmail.com>
2020-09-18 17:46:59 +0200

    patch the main.lua file

--- make.sh
+++ make.sh
@@ -9,24 +9,21 @@ function process
     local BIN_DIR="${VSCODE_LUA_DIR}/bin/$2"
     echo "ensuring directories exist: ${OUT_DIR}"
     mkdir -p "${OUT_DIR}"
-    echo "entering directory: ${OUT_DIR}"
-    pushd "${OUT_DIR}"
-        mkdir -p bin/$2
-        cp "${BIN_DIR}"/* bin/$2/
-        chmod +x bin/$2/*
-        cp -R "${VSCODE_LUA_DIR}/server/libs" .
-        cp -R "${VSCODE_LUA_DIR}/server/locale" .
-        cp -R "${VSCODE_LUA_DIR}/server/script" .
-        cp "${VSCODE_LUA_DIR}/server/main.lua" .
-        cp "${VSCODE_LUA_DIR}/server/platform.lua" .
-        cp "${LSP_LUA_SOURCE_DIR}/plugin.py" .
-        cp "${LSP_LUA_SOURCE_DIR}/LSP-lua.sublime-commands" .
-        cp "${LSP_LUA_SOURCE_DIR}/LSP-lua.sublime-settings" .
-        cp "${LSP_LUA_SOURCE_DIR}/LICENSE" .
-        cp "${LSP_LUA_SOURCE_DIR}/NOTICE" .
-        touch .no-sublime-package
-    popd
-    echo "exited directory ${OUT_DIR}"
+    mkdir -p bin/$2
+    cp "${BIN_DIR}"/* bin/$2/
+    chmod +x bin/$2/*
+    cp -R "${VSCODE_LUA_DIR}/server/libs" "${OUT_DIR}/"
+    cp -R "${VSCODE_LUA_DIR}/server/locale" "${OUT_DIR}/"
+    cp -R "${VSCODE_LUA_DIR}/server/script" "${OUT_DIR}/"
+    cp "${VSCODE_LUA_DIR}/server/main.lua" "${OUT_DIR}/"
+    cp "${VSCODE_LUA_DIR}/server/platform.lua" "${OUT_DIR}/"
+    cp "${LSP_LUA_SOURCE_DIR}/plugin.py" "${OUT_DIR}/"
+    cp "${LSP_LUA_SOURCE_DIR}/LSP-lua.sublime-commands" "${OUT_DIR}/"
+    cp "${LSP_LUA_SOURCE_DIR}/LSP-lua.sublime-settings" "${OUT_DIR}/"
+    cp "${LSP_LUA_SOURCE_DIR}/LICENSE" "${OUT_DIR}/"
+    cp "${LSP_LUA_SOURCE_DIR}/NOTICE" "${OUT_DIR}/"
+    touch "${OUT_DIR}/.no-sublime-package"
+    patch "${OUT_DIR}/main.lua" "${LSP_LUA_SOURCE_DIR}/patch.diff"
     zip LSP-lua-$1.zip "${OUT_DIR}"
 }
 
