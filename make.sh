48e622fcb153d2206bd32af219acc50cf2f93e4f
Raoul Wols <raoulwols@gmail.com>
2020-09-18 17:52:13 +0200

    Github workflows

---
+++ make.sh
@@ -0,0 +1,34 @@
+set -e
+
+VSCODE_LUA_DIR="${GITHUB_WORKSPACE}/vscode-lua"
+LSP_LUA_SOURCE_DIR="${GITHUB_WORKSPACE}/LSP-lua-source"
+
+function process
+{
+    local OUT_DIR="${GITHUB_WORKSPACE}/out/LSP-lua-$1"
+    local BIN_DIR="${VSCODE_LUA_DIR}/bin/$2"
+    echo "ensuring directories exist: ${OUT_DIR}"
+    mkdir -p "${OUT_DIR}"
+    mkdir -p bin/$2
+    cp "${BIN_DIR}"/* bin/$2/
+    chmod +x bin/$2/*
+    cp -R "${VSCODE_LUA_DIR}/server/libs" "${OUT_DIR}/"
+    cp -R "${VSCODE_LUA_DIR}/server/locale" "${OUT_DIR}/"
+    cp -R "${VSCODE_LUA_DIR}/server/script" "${OUT_DIR}/"
+    cp "${VSCODE_LUA_DIR}/server/main.lua" "${OUT_DIR}/"
+    cp "${VSCODE_LUA_DIR}/server/platform.lua" "${OUT_DIR}/"
+    cp "${LSP_LUA_SOURCE_DIR}/plugin.py" "${OUT_DIR}/"
+    cp "${LSP_LUA_SOURCE_DIR}/LSP-lua.sublime-commands" "${OUT_DIR}/"
+    cp "${LSP_LUA_SOURCE_DIR}/LSP-lua.sublime-settings" "${OUT_DIR}/"
+    cp "${LSP_LUA_SOURCE_DIR}/LICENSE" "${OUT_DIR}/"
+    cp "${LSP_LUA_SOURCE_DIR}/NOTICE" "${OUT_DIR}/"
+    touch "${OUT_DIR}/.no-sublime-package"
+    patch "${OUT_DIR}/main.lua" "${LSP_LUA_SOURCE_DIR}/patch.diff"
+    zip LSP-lua-$1.zip "${OUT_DIR}"
+}
+
+process linux Linux
+process osx macOS
+process windows Windows
+
+ls -lash
