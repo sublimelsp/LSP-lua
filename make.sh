8ed44efe0ffe2f2d3bfd5b49896ccf263e0d39f3
Raoul Wols <raoulwols@gmail.com>
2020-09-18 17:39:02 +0200

    debug

--- make.sh
+++ make.sh
@@ -7,7 +7,9 @@ function process
 {
     local OUT_DIR="${GITHUB_WORKSPACE}/out/LSP-lua-$1"
     local BIN_DIR="${VSCODE_LUA_DIR}/bin/$2"
+    echo "ensuring directories exist: ${OUT_DIR}"
     mkdir -p "${OUT_DIR}"
+    echo "entering directory: ${OUT_DIR}"
     pushd "${OUT_DIR}"
         mkdir -p bin/$2
         cp "${BIN_DIR}"/* bin/$2/
@@ -16,7 +18,7 @@ function process
         cp -R "${VSCODE_LUA_DIR}/server/locale" .
         cp -R "${VSCODE_LUA_DIR}/server/script" .
         cp "${VSCODE_LUA_DIR}/server/main.lua" .
-        cp -R "${VSCODE_LUA_DIR}/server/platform.lua" .
+        cp "${VSCODE_LUA_DIR}/server/platform.lua" .
         cp "${LSP_LUA_SOURCE_DIR}/plugin.py" .
         cp "${LSP_LUA_SOURCE_DIR}/LSP-lua.sublime-commands" .
         cp "${LSP_LUA_SOURCE_DIR}/LSP-lua.sublime-settings" .
@@ -24,6 +26,7 @@ function process
         cp "${LSP_LUA_SOURCE_DIR}/NOTICE" .
         touch .no-sublime-package
     popd
+    echo "exited directory ${OUT_DIR}"
     zip LSP-lua-$1.zip "${OUT_DIR}"
 }
 
