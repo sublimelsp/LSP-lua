1645485c814cb0df4890b504659bd0e0098dd5cf
Raoul Wols <raoulwols@gmail.com>
2020-09-18 21:37:10 +0200

    tweaks

--- make.sh
+++ make.sh
@@ -1,17 +1,15 @@
 set -e
 
-VSCODE_LUA_DIR="${GITHUB_WORKSPACE}/vscode-lua"
-LSP_LUA_SOURCE_DIR="${GITHUB_WORKSPACE}/LSP-lua-source"
+VSCODE_LUA_DIR="${GITHUB_WORKSPACE}/$1"
+LSP_LUA_SOURCE_DIR="${GITHUB_WORKSPACE}/$2"
 
 function process
 {
     local OUT_DIR="${GITHUB_WORKSPACE}/out/LSP-lua-$1"
     local BIN_DIR="${VSCODE_LUA_DIR}/bin/$2"
-    echo "ensuring directories exist: ${OUT_DIR}"
-    mkdir -p "${OUT_DIR}"
-    mkdir -p bin/$2
+    mkdir -p "${OUT_DIR}/bin/$2"
     cp "${BIN_DIR}"/* bin/$2/
-    chmod +x bin/$2/*
+    chmod +x "${OUT_DIR}"/bin/$2/*
     cp -R "${VSCODE_LUA_DIR}/server/libs" "${OUT_DIR}/"
     cp -R "${VSCODE_LUA_DIR}/server/locale" "${OUT_DIR}/"
     cp -R "${VSCODE_LUA_DIR}/server/script" "${OUT_DIR}/"
@@ -31,4 +29,6 @@ process linux Linux
 process osx macOS
 process windows Windows
 
-ls -lash
+ls -lash LSP-lua-linux.zip
+ls -lash LSP-lua-osx.zip
+ls -lash LSP-lua-windows.zip
