a84278c089eff2d878329171689e97d4c2adaac7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-21 15:09:33 +0000

    Adding tarfile member sanitization to extractall()

--- plugin.py
+++ plugin.py
@@ -65,7 +65,26 @@ class Lua(AbstractPlugin):
                         z.extractall(tmp)
                 else:
                     with tarfile.open(downloaded_file) as z:
-                        z.extractall(tmp)
+                        def is_within_directory(directory, target):
+                            
+                            abs_directory = os.path.abspath(directory)
+                            abs_target = os.path.abspath(target)
+                        
+                            prefix = os.path.commonprefix([abs_directory, abs_target])
+                            
+                            return prefix == abs_directory
+                        
+                        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                        
+                            for member in tar.getmembers():
+                                member_path = os.path.join(path, member.name)
+                                if not is_within_directory(path, member_path):
+                                    raise Exception("Attempted Path Traversal in Tar File")
+                        
+                            tar.extractall(path, members, numeric_owner=numeric_owner) 
+                            
+                        
+                        safe_extract(z, tmp)
                 os.makedirs(cls.storage_path(), exist_ok=True)
                 copy_tree(os.path.join(tmp), cls.basedir())
             with open(cls.version_file(), "w") as fp:
